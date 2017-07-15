da694c4f33f3cecccd6a86c4a3dfa5518caca375
kadav8 <kadav8@gmail.com>
2017-07-15 08:01:03 +0200

    javítás

--- spring-demoapp/src/main/java/com/example/springapp/DemoApplication.java
+++ spring-demoapp/src/main/java/com/example/springapp/DemoApplication.java
@@ -2,7 +2,6 @@ package com.example.springapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication

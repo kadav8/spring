96ae9b6bc97b90bdf7bee2efcaa9df2da704b7d5
Dávid Kaszányi <kadav8@gmail.com>
2019-03-13 19:31:19 +0100

    camunda

---
+++ spring-camunda/src/main/java/com/example/camunda/CamundaCoreappApplication.java
@@ -0,0 +1,13 @@
+package com.example.camunda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaCoreappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamundaCoreappApplication.class, args);
+	}
+
+}
